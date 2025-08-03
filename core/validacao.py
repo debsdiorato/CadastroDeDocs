5286fcd40c2ba986da416df3f4268bd612dd0adf
Debora Diorato <129698967+debsdiorato@users.noreply.github.com>
2025-08-03 16:50:10 -0300

    Criado esqueleto do projeto.
    Criando esqueleto das pastas do projeto para se manter organizado e de facil entendimento, criada janela inicial com tk para interagir com usuario.

---
+++ core/validacao.py
@@ -0,0 +1,1 @@
+#verificando datas, campos vazios. validacoes
---
+++ main.py
@@ -0,0 +1,12 @@
+#arquivo principal
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+
+    root.title("Cadastro de Documentos")
+    root.geometry("800x600")
+    root.mainloop() #loop para interacao do usuario
+
+if __name__ == "__main__":
+    main()
